23315ef2a7443f6ae001889f1c3c5e1234fb924e
Pedro <pedrodmbc@gmail.com>
2019-04-11 11:22:14 -0300

    changes to view infos in frontend

--- src/models/File.js
+++ src/models/File.js
@@ -14,8 +14,17 @@ const File = new mongoose.Schema(
   }, 
   {
     // cria um campo chamado createdAt e updatedAt em cada registro
-    timestamps: true
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
   }
 )
+
+// campo virtual para exibir info para frontend diretamente
+// precisa ser function normal para ter acesso ao this (se refere ao 'const File' registro de arquivo)
+// encondeURIComponent - o texto fica em formato de URL
+File.virtual('url').get(function(){
+  return `http://localhost:3333/files/${encodeURIComponent(this.path)}`
+})
 
 module.exports = mongoose.model("File", File)
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
@@ -12,7 +13,11 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-wqbm5.mongodb.net/o
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// redirect - toda vez que o user acessar a rota files, o servidor vai buscar os arquivos que tem na pasta 'tmp'
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'))
 
+// vars ambiente: podem sobrescrever infos dentro da app dependendo do ambiente que estamos usando
+// a var PORT é definida automaticamente pelo heroku (process.event.PORT || )
 app.listen(3333)
